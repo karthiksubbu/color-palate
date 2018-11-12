063dfde0fe2294032a30d941220109e55421614b
developerk786 <developerk786@gmail.com>
2018-11-12 12:37:00 +0545

    Initial Setup Done #React

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Hello React!</h4>
+      </div>
+    );
+  }
+}
+
+export default App;
