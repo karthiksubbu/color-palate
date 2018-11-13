ef5cec4c8701f6309c65af55b739074984daf44d
developerk786 <developerk786@gmail.com>
2018-11-13 19:53:32 +0545

    build files added

--- src/app/store/createReducer.js
+++
@@ -1,6 +0,0 @@
-export default (initState, fnMap) => {
-  return (state = initState, { type, payload }) => {
-    const handler = fnMap[type];
-    return handler ? handler(state, payload) : state;
-  };
-};
