5881402038a63b93a55871b3a9f3e5dc059f977d
developerk786 <developerk786@gmail.com>
2018-11-13 20:22:51 +0545

    Bug fixed build

---
+++ precache-manifest.28cbe1d4f728d22f8b2a31d84690afe7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f62500a6b012f1fb70a",
+    "url": "/color-palate/static/js/runtime~main.0f62500a.js"
+  },
+  {
+    "revision": "05b515b4d97b8503a524",
+    "url": "/color-palate/static/js/main.05b515b4.chunk.js"
+  },
+  {
+    "revision": "09a628aeebada43848a4",
+    "url": "/color-palate/static/js/1.09a628ae.chunk.js"
+  },
+  {
+    "revision": "05b515b4d97b8503a524",
+    "url": "/color-palate/static/css/main.830d3733.chunk.css"
+  },
+  {
+    "revision": "0d92a227342d930e3707a6b642d65e3e",
+    "url": "/color-palate/index.html"
+  }
+];
